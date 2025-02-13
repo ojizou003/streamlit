a23d583c6e4b16540becf681b8938b53327fc00f
ojizou003 <sinissi@gmail.com>
2025-02-14 07:37:34 +0900

    7th_commit

--- ojizou_app/main_app.py
+++ ojizou_app/main_app.py
@@ -5,6 +5,7 @@ from PIL import Image
 st.title('ojizouアプリ')
 st.caption('これはojizouのテスト用アプリです。')
 
-image = Image.open('./data/my-notion-face-transparent.png')
+# image = Image.open('./data/my-notion-face-transparent.png')
+image = Image.open('/mount/src/streamlit/ojizou_app/data/my-notion-face-transparent.png')
 
 st.image(image, width=200)
